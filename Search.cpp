e2881bde62c5ff94ff0c949f82be36d19eba7b7d
Bryan <remdul@gmail.com>
2014-12-15 15:54:27 -0500

    Additional changes to search

--- Search.cpp
+++ Search.cpp
@@ -5,29 +5,20 @@
 #include <vector>
 #include <sstream>
 
-std::string rawData(std::vector<Entry> entries)
-{
+std::string rawData(std::vector<Entry> entries) {
     std::ostringstream stream;
-    for (auto iter : entries)
-    {
-   stream
-        << iter.ID << ", "
-        << iter.devName << ", "
-        << iter.PSID << ", "
-        << iter.entryType << ", "
-        << iter.entryTime << ", "
-        << iter.termStatus << ", "
-        << iter.exitStatus << ", "
-        << iter.hostName << "\n";
+    for (auto iter : entries) {
+        stream << iter.ID << ", " << iter.devName << ", " << iter.PSID << ", "
+                << iter.entryType << ", " << iter.entryTime << ", "
+                << iter.termStatus << ", "
+//        << iter.exitStatus << ", "
+                << iter.hostName << "\n";
     }
+    return stream.str();
 }
 
-auto search(std::string searchString, std::vector<Entry> entries)
-{
-    pcrecpp::RE regex(R"!(^(.+?),(.+?),\s*([\d\.]+)(KM|M),\s*"(.+)")!");
-
-    if (!regex.PartialMatch(line)) {
-        cout << "Could not parse ->" << line << "<-" << endl;
-        throw string("Error");
-
+auto search(std::string searchString, std::vector<Entry> entries) {
+    pcrecpp::RE regex(R"!(^.+)()()!");
+    auto test;
+    return test;
 }
