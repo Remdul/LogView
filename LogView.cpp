7e27e7e956511fdb68dff39dbd80b63676a3ddab
explicitname <explicit6911@gmail.com>
2014-12-15 11:06:14 -0500

    Base for LogView.cpp

--- LogView.cpp
+++ LogView.cpp
@@ -1,1 +1,22 @@
 // First Commit
+
+#include <iostream>
+#include <ifstream>
+#include <ofstream>
+
+using namespace std;
+
+int main()
+
+{
+
+	ifstream wtmp;
+	ifstream utmp;
+
+
+
+
+
+
+	return 0;
+}
