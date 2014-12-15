33c610e5a8c9f00b265a2ec6ed0ac783b8de706f
Bryan <remdul@gmail.com>
2014-12-15 13:58:34 -0500

    ugh

--- LogView.cpp
+++ LogView.cpp
@@ -1,21 +1,34 @@
-// First Commit
-
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
-
 {
-	ifstream wtmp("/var/log/wtmp", ios::binary | ios::in);
-	ifstream utmp;
+    unsigned char ch;
 
+/*  '0' = 0x30 = %00110000
+    '1' = 0x31 = %00110001   */
 
-    char * x;
-    wtmp.open("silly.dat", ios::binary | ios::in);
-    wtmp.read(x, 4082);
-    std::cout << x << std::endl;
-	return 0;
+    ofstream ostream("test.txt", ios::binary);
+    ostream.write("1", 1);
+    ostream.close();
 
+    ifstream istream("/var/log/wtmp");
+    while (ch = istream.get() != NULL) {
+        printf("%d", (ch & 0x80) / 0x80);
+        printf("%d", (ch & 0x40) / 0x40);
+        printf("%d", (ch & 0x20) / 0x20);
+        printf("%d", (ch & 0x10) / 0x10);
+        printf("%d", (ch & 0x08) / 0x08);
+        printf("%d", (ch & 0x04) / 0x04);
+        printf("%d", (ch & 0x02) / 0x02);
+        printf("%d\n", (ch & 0x01) / 0x01);
+        //Output: 00110001
+    }
+    istream.close();
+
+    cin.ignore();
+    return 0;
 }
