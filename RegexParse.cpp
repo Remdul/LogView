bb04ef766b80b25ddf5fb7366a78b9b13a91adf2
Bryan <remdul@gmail.com>
2014-12-15 12:02:46 -0500

    Beginning parsing development. Commiting to pull new line reader.

--- RegexParse.cpp
+++ RegexParse.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include <string>
 #include <pcrecpp.h>
+#include "LogView.h"
+
+Entries parseFile(std::string line)
+{
+    std::string userName;
+    std::string ID;
+    std::string devName;
+    int PSID;
+    std::string entryType;
+    std::string entryTime;
+    std::string termStatus;
+    std::string exitStatus;
+    std::string hostName;
+
+    pcrecpp::RE regex(R"!(^(.+?),(.+?),\s*([\d\.]+)(KM|M),\s*"(.+)")!");
+    if (!regex.PartialMatch(line, &userName)) {
+       // cout << "Could not parse ->" << line << "<-" << endl;
+        throw string("Error");
+    }
+
+    Entries newEntry;
+    newEntry.setEntries(userName, ID, devName, PSID, entryType, entryTime, termStatus, exitStatus, hostName);
+    return newEntry;
+}
+
 
 
