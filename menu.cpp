e9eaa890e1212e5d812864e6fe436e6c1602e6a4
JMcLaurin <jmclaurin25@gmail.com>
2014-12-15 16:05:05 -0500

    Removed Term status from menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -7,7 +7,7 @@ void menuHeader() {
     std::cout << std::setw(12) << "User Name" << std::setw(6) << "ID"
             << std::setw(12) << "Dev Name" << std::setw(6) << "PSID"
             << std::setw(12) << "Entry Type" << std::setw(14) << "Entry Time"
-            << std::setw(14) << "Termination" << std::setw(14) << "Exit Status"
+            << std::setw(14) << "Exit Status"
             << std::setw(12) << "Host Name" << std::endl;
 }
 
@@ -16,6 +16,6 @@ void menuBody(Entry blockInput) {
             << blockInput.ID << std::setw(12) << blockInput.devName
             << std::setw(6) << blockInput.PSID << std::setw(12)
             << blockInput.entryType << std::setw(14) << blockInput.entryTime
-            << std::setw(14) << blockInput.termStatus << std::setw(14)
-            << blockInput.exitStatus << std::setw(12) << blockInput.hostName << std::endl;
+            << std::setw(14) << blockInput.exitStatus << std::setw(12)
+    		<< blockInput.hostName << std::endl;
 }
