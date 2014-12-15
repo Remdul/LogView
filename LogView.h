05b9d1463bced7bfe0bf7b55423b8ed817fc4620
JMcLaurin <jmclaurin25@gmail.com>
2014-12-15 11:10:30 -0500

    Entries - Class creation

---
+++ LogView.h
@@ -0,0 +1,27 @@
+#ifndef LOGVIEW_H_
+#define LOGVIEW_H_
+
+#include <iostream>
+#include <string>
+
+class Entries
+{
+public:
+    Entries(){}
+    void setEntries(std::string newUserName, std::string newID, std::string newDevName, int newPSID, std::string newEntryType, std::string newEntryTime, std::string newTermStatus, std::string newExitStatus, std::string newHostName);
+    void printEntries();
+private:
+		std::string userName;
+		std::string ID;
+		std::string devName;
+		int PSID;
+		std::string entryType;
+		std::string entryTime;
+		std::string termStatus;
+		std::string exitStatus;
+		std::string hostName;
+};
+
+Entries::Entries ();
+
+#endif /* LOGVIEW_H_ */
