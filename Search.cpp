ac2b945ecd8914bbbe07431cbc8029bfbe820361
Bryan <remdul@gmail.com>
2014-12-16 11:16:24 -0500

    Adding fancier search stuffs.

--- Search.cpp
+++ Search.cpp
@@ -5,14 +5,12 @@
 #include <vector>
 #include <sstream>
 
-std::string hexToAscii(std::string hex)
-{
+std::string hexToAscii(std::string hex) {
     int len = hex.length();
     std::string newString;
-    for(int i=0; i< len; i+=2)
-    {
-        string byte = hex.substr(i,2);
-        char chr = (char) (int)strtol(byte.c_str(), NULL, 16);
+    for (int i = 0; i < len; i += 2) {
+        string byte = hex.substr(i, 2);
+        char chr = (char) (int) strtol(byte.c_str(), NULL, 16);
         newString.push_back(chr);
     }
     return newString;
@@ -21,13 +19,24 @@ std::string hexToAscii(std::string hex)
 std::string rawData(std::vector<utmp> blockEntries) {
     std::ostringstream stream;
     for (auto iter : blockEntries) {
-        stream << iter.ut_user << ", "  << iter.ut_pid << ", " << iter.ut_host << ", " << iter.ut_session << ", " << iter.ut_addr_v6 << ", " << iter.ut_id << ", " << iter.ut_line << ", " << iter.ut_exit.e_exit << ", " << iter.ut_exit.e_termination << ", " << iter.ut_tv.tv_sec << "\n";
+        stream << iter.ut_user << ", " << iter.ut_pid << ", " << iter.ut_host
+                << ", " << iter.ut_session << ", " << iter.ut_addr_v6 << ", "
+                << iter.ut_id << ", " << iter.ut_line << ", "
+                << iter.ut_exit.e_exit << ", " << iter.ut_exit.e_termination
+                << ", " << iter.ut_tv.tv_sec << "\n";
     }
     return stream.str();
 }
 /*
-auto search(std::string searchString, std::vector<Entry> entries) {
-    pcrecpp::RE regex(R"!(^.+)()()!");
-    auto test;
-    return test;
-}*/
+ * reboot, 0, 3.13.0-40-generic, 0, 0x7fff5bcc427c, ~~, ~, 0, 0, 1418589862
+ * bryan, 4863, :0, 0, 0x7fff5bcc427c, /3, pts/3, 0, 0, 1418652552
+ */
+auto search(std::string searchString, std::vector<utmp> entries) {
+    auto findGroup = 10;
+    pcrecpp::RE regex(s earchString);
+    if (!regex.PartialMatch(rawData(entries))) {
+        std::cout << "Could not parse data with search string: " << searchString << std::endl;
+        throw string("Error");
+    }
+    return findGroup;
+}
