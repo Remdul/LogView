9fc9e94cd3e1da170d270536c38280e027359ab4
explicitname <explicit6911@gmail.com>
2014-12-16 13:23:07 -0500

    Adding menu and looping functions

--- LogView.cpp
+++ LogView.cpp
@@ -6,55 +6,65 @@
 #include <vector>
 #include "LogView.h"
 /*
-void menuSelection(std::string x)
-{
-	switch (x)
-	{
-	case 'Search':
-		std::cout << "Search For Stuff!" << std::endl;
-		break;
-	case 'Exit Program':
-		std::cout << "Exit Program Stuff!" << std::endl;
-		break;
-	case 'Print Menu':
-		std::cout << "Print Menu Stuff!" << std::endl;
-		break;
-	case 'Export':
-		std::cout << "Export Stuff!" << std::endl;
-		break;
-	default:
-		std::cout << "Blargh!" << std::endl;
-	}
-}
-*/
+ void menuSelection(int x)
+ {
+ switch (x)
+ {
+ case '1':
+ std::cout << "Search For Stuff!" << std::endl;
+ break;
+ case '2':
+ std::cout << "Export Stuff!" << std::endl;
+ break;
+ case '3':
+ std::cout << "Result Stuff!" << std::endl;
+ break;
+ case '4':
+ std::cout << "Exit Stuff!" << std::endl;
+ break;
+ default:
+ std::cout << "Blargh!" << std::endl;
+ }
+ }
+ */
 
 int main()
 {
 	std::ifstream utmpFile("/var/run/utmp", std::ifstream::binary);
 	std::ifstream wtmpFile("/var/log/wtmp", std::ifstream::binary);
 	std::vector<utmp> blockEntries;
-	if (utmpFile)
+	std::string cont = "yes";
+
+	while(cont == "yes" || cont == "Yes")
 	{
-		// get length of file:
-		utmpFile.seekg(0, utmpFile.end);
-		utmpFile.seekg(0, utmpFile.beg);
-		utmp blockEntry;
-		// read data as a block:
-		while (utmpFile)
+		std::cout << "Please enter the appropriate number. " << std::endl << std::endl;
+		std::cout << "[1] Search " << std::endl;
+		std::cout << "[2] Export to file" << std::endl;
+		std::cout << "[3] "
+
+		if (utmpFile)
 		{
-			utmpFile.read((char *) &blockEntry, sizeof(utmp));
-			// std::cout << "Reading " << sizeof(utmp) << " characters... "
-			//		<< std::endl;
-			blockEntries.push_back(blockEntry);
+			// get length of file:
+			utmpFile.seekg(0, utmpFile.end);
+			utmpFile.seekg(0, utmpFile.beg);
+			utmp blockEntry;
+			// read data as a block:
+			while (utmpFile)
+			{
+				utmpFile.read((char *) &blockEntry, sizeof(utmp));
+				// std::cout << "Reading " << sizeof(utmp) << " characters... "
+				//		<< std::endl;
+				blockEntries.push_back(blockEntry);
+			}
+
+			menuOutput(blockEntries);
+
+			std::cout << "Read in " << blockEntries.size() << " blocks."
+					<< std::endl;
+			std::cout << rawData(blockEntries) << std::endl;
+
+			utmpFile.close();
 		}
-
-		menuOutput(blockEntries);
-
-		std::cout << "Read in " << blockEntries.size() << " blocks."
-				<< std::endl;
-		std::cout << rawData(blockEntries) << std::endl;
-
-		utmpFile.close();
 	}
 	return 0;
 }
