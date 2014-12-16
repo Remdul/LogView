f6f6511f0674e6c51eb7e9e0d73ecfc48c55597c
JMcLaurin <jmclaurin25@gmail.com>
2014-12-16 09:42:07 -0500

    Combined Menu functions (2) into (1) function.

--- menu.cpp
+++ menu.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+#include <utmp.h>
 #include "LogView.h"
 
 void menuHeader() {
-    std::cout << std::setw(12) << "User Name" << std::setw(6) << "ID"
-            << std::setw(12) << "Dev Name" << std::setw(6) << "PSID"
-            << std::setw(12) << "Entry Type" << std::setw(14) << "Entry Time"
-            << std::setw(14) << "Exit Status"
-            << std::setw(12) << "Host Name" << std::endl;
+    std::cout << std::setw(15) << "User Name" << std::setw(10) << "ID"
+            << std::setw(15) << "Dev Name" << std::setw(10) << "PSID"
+            << std::setw(15) << "Entry Type" << std::setw(15) << "Entry Time"
+            << std::setw(15) << "Exit Status"
+            << std::setw(15) << "Host Name" << std::endl;
 }
 
 void menuBody(Entry blockInput) {
-    std::cout << std::setw(12) << blockInput.userName << std::setw(6)
-            << blockInput.ID << std::setw(12) << blockInput.devName
-            << std::setw(6) << blockInput.PSID << std::setw(12)
-            << blockInput.entryType << std::setw(14) << blockInput.entryTime
-            << std::setw(14) << blockInput.exitStatus << std::setw(12)
-    		<< blockInput.hostName << std::endl;
+    std::cout << std::setw(15) << blockInput.ut_user << std::setw(10)
+            << blockInput.ut_id << std::setw(15) << blockInput.ut_line
+            << std::setw(10) << blockInput.ut_pid << std::setw(15)
+            << blockInput.ut_type << std::setw(15) << blockInput.ut_tv
+            << std::setw(15) << blockInput.ut_exit << std::setw(15)
+    		<< blockInput.ut_host << std::endl;
+}
+
+void menuOutput(std::vector blockVector) {
+	menuHeader();
+
+	for (auto iter : blockVector) {
+		menuBody(iter);
+	}
 }
