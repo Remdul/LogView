f0a160c009b4259a28dd47a5af676927037e5832
Bryan <remdul@gmail.com>
2014-12-16 09:51:25 -0500

    Cleaning up merges.

--- LogView.h
+++ LogView.h
@@ -6,32 +6,6 @@
 #include <vector>
 #include <utmp.h>
 
-<<<<<<< HEAD
-class Entry {
-public:
-    Entry() {}
-    void setEntry(char newUserName, char newID, char newDevName, pid_t newPSID,
-            short newEntryType, int32_t newEntryTime,             char newHostName);
-    void printEntry();
-private:
-    char userName;
-    char ID;
-    char devName;
-    pid_t PSID;
-    short entryType;
-    int32_t entryTime;
-    char hostName;
-=======
-class
-{
-
-public:
-
-private:
-
->>>>>>> e2be7d77afb102e2605b2b1d148fb1d6bfef322d
-};
-
 void menuOutput(std::vector blockVector);
 
 #endif /* LOGVIEW_H_ */
