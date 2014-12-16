880a6f58861074644e96283f803f5e52b51a521a
explicitname <explicit6911@gmail.com>
2014-12-16 11:18:06 -0500

    Failed attempt at epoch time conversion.

--- Utils.cpp
+++ Utils.cpp
@@ -4,7 +4,22 @@
  *  Created on: Dec 16, 2014
  *      Author: explicitness
  */
+#include <iostream>
+#include <ctime>
 
+int epochConvert (x)
+{
+  time_t epochTime;
+  struct tm * ptm;
+
+  time ( &epochTime );
+
+  ptm = gmtime ( &epochTime );
+
+  std::cout << "Current time around the World:" << std::endl;
+
+  return 0;
+}
 
 
 
